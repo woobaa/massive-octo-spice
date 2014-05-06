5b92d397597c19f6881bf70dfcf1155415f502b6
Woobaa <woobaa.github@gmail.com>
2014-05-06 20:51:16 +0200

    Breaking up the code Pt. 1

--- main.cpp
+++ main.cpp
@@ -3,26 +3,44 @@
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-int main() 
+bool g_bRunning = false;
+
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
     {
-        g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL", 
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+        g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
 
         if (g_pWindow != 0)
             g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
     }
     else
-        return 1;
+        return false;
 
+    return true;
+}
+
+void render()
+{
     SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
     SDL_RenderClear(g_pRenderer);
 
     SDL_RenderPresent(g_pRenderer);
+}
 
-    SDL_Delay(5000);
+int main() 
+{
+    if (init("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+        g_bRunning = true;
+    else
+        return 1;
+
+    while (g_bRunning)
+    {
+        render();
+    }
 
     SDL_Quit();
 
